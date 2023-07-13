ae9714d588073912ff7b55ce4300e8f8074d03c3
thirumurugan7 <thiru82003@gmail.com>
2023-07-13 19:23:44 +0530

    finished read and write

--- models/StudentIntro.ts
+++ models/StudentIntro.ts
@@ -1,3 +1,6 @@
+import * as borsh from '@project-serum/borsh'
+
+
 export class StudentIntro {
     name: string;
     message: string;
@@ -7,9 +10,35 @@ export class StudentIntro {
         this.message = message;
     }
 
-    static mocks: StudentIntro[] = [
-        new StudentIntro('Elizabeth Holmes', `Learning Solana so I can use it to build sick NFT projects.`),
-        new StudentIntro('Jack Nicholson', `I want to overhaul the world's financial system. Lower friction payments/transfer, lower fees, faster payouts, better collateralization for loans, etc.`),
-        new StudentIntro('Terminator', `i'm basically here to protect`),
-    ]
+    static borshAccountSchema = borsh.struct([
+        borsh.bool('initialized'),
+        borsh.str('name'),
+        borsh.str('message')
+       ])
+
+    static deserialize(buffer?: Buffer): StudentIntro|null{
+        if(!buffer){
+            return null
+        }
+
+        try{
+            const { name, message} = this.borshAccountSchema.decode(buffer)
+            return new StudentIntro(name,message)
+        } catch(e){
+            console.log("deserialize error:", e);
+            return null
+        }
+    }   
+
+    borshInstructionSchema = borsh.struct([
+        borsh.u8('variant'),
+        borsh.str('name'),
+        borsh.str('message'),
+    ])
+
+    serialize(): Buffer {
+        const buffer = Buffer.alloc(1000);
+        this.borshInstructionSchema.encode({...this, variant: 0}, buffer);
+        return buffer.slice(0,this.borshInstructionSchema.getSpan(buffer))
+    }
 }
